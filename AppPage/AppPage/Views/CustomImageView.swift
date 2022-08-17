7ea50c63f62c1694c61295b4398800eaa20d906c
jogiwan <turu801@flow.team>
2022-08-17 19:13:34 +0900

    ✨ CustomImageView 추가

---
+++ AppPage/AppPage/Views/CustomImageView.swift
@@ -0,0 +1,47 @@
+//
+//  CustomImageView.swift
+//  AppPage
+//
+//  Created by Giwan Jo on 2022/08/17.
+//
+
+import Combine
+import UIKit
+
+final class CustomImageView: UIImageView {
+    private var cancelBag = Set<AnyCancellable>()
+    
+    func setImage(url: URL) {
+        let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad)
+        URLSession.shared.publisher(for: request)
+            .catch({ (error: Error) -> AnyPublisher<Result<URLSession.DataTaskPublisher.Output, Error>, Error> in
+                switch error {
+                case ServiceError.transportError:
+                    return Fail(error: error)
+                        .delay(for: 3, scheduler: DispatchQueue.main)
+                        .eraseToAnyPublisher()
+                default:
+                    return Just(Result.failure(error))
+                        .setFailureType(to: Error.self)
+                        .eraseToAnyPublisher()
+                }
+            })
+            .retry(2)
+            .tryMap {
+                return try $0.get().data
+            }
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(_):
+                    self.image = UIImage(systemName: "wifi.exclamationmark")
+                case .finished:
+                    break
+                }
+            } receiveValue: { data in
+                self.image = UIImage(data: data)
+            }
+            .store(in: &cancelBag)
+    }
+}
+
